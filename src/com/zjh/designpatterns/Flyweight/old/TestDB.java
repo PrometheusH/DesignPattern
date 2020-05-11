f7f738d7f2f69811fa7d6e333972fb8f551fe9d2
zhoujiahui <zhoujiahuimaster@126.com>
2020-05-11 18:17:02 +0800

    享元模式模拟数据类

---
+++ src/com/zjh/designpatterns/Flyweight/old/TestDB.java
@@ -0,0 +1,25 @@
+package com.zjh.designpatterns.Flyweight.old;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * 供测试用，在内存中模拟数据库的值
+ */
+public class TestDB {
+    //用来存放授权数据的值
+    public static Collection<String> colDB=new ArrayList<String>();
+
+    static {
+        //通过静态块来填充模拟的数据
+        colDB.add("张三，人员列表，查看");
+        colDB.add("李四，人员列表，查看");
+        colDB.add("李四，薪资数据，查看");
+        colDB.add("李四，薪资数据，修改");
+
+        //增加更过的授权数据
+        for (int i=0;i<3;i++){
+            colDB.add("张三"+i+",人员列表，查看");
+        }
+    }
+}
