f8a4a5818db812c2ee6af4683917f68e21246b04
zhoujiahui <zhoujiahuimaster@126.com>
2020-05-11 19:19:56 +0800

    享元模式修正了登陆找不到人的bug，因为逗号是中文的，不能切分的原因

--- src/com/zjh/designpatterns/Flyweight/old/TestDB.java
+++ src/com/zjh/designpatterns/Flyweight/old/TestDB.java
@@ -12,14 +12,14 @@ public class TestDB {
 
     static {
         //通过静态块来填充模拟的数据
-        colDB.add("张三，人员列表，查看");
-        colDB.add("李四，人员列表，查看");
-        colDB.add("李四，薪资数据，查看");
-        colDB.add("李四，薪资数据，修改");
+        colDB.add("张三,人员列表,查看");
+        colDB.add("李四,人员列表,查看");
+        colDB.add("李四,薪资数据,查看");
+        colDB.add("李四,薪资数据,修改");
 
         //增加更过的授权数据
         for (int i=0;i<3;i++){
-            colDB.add("张三"+i+",人员列表，查看");
+            colDB.add("张三"+i+",人员列表,查看");
         }
     }
 }
