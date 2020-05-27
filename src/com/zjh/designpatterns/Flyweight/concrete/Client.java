3e55b4b8926703b02642d6e49dbebcee1562f8f8
zhoujiahui <zhoujiahuimaster@126.com>
2020-05-27 16:02:38 +0800

    无更新

--- src/com/zjh/designpatterns/Flyweight/concrete/Client.java
+++ src/com/zjh/designpatterns/Flyweight/concrete/Client.java
@@ -3,11 +3,10 @@ package com.zjh.designpatterns.Flyweight.concrete;
 import com.zjh.designpatterns.Flyweight.concrete.SecurityMgr;
 
 public class Client {
-    //呜哈哈哈哈，测试bbbbbb
+
     public static void main(String[] args) {
         //需要先登陆，然后再判断是否有权限
         SecurityMgr mgr = SecurityMgr.getInstance();
-        //这里做一次测试aaaaaa
 
         mgr.login("张三");
         mgr.login("李四");
@@ -19,7 +18,7 @@ public class Client {
 
         for (int i=0;i<3;i++){
             mgr.login("张三"+i);
-            System.out.println(mgr.hasPermit("张三"+i,"沃特测试CCC数据","查看"));
+            System.out.println(mgr.hasPermit("张三"+i,"测试数据","查看"));
         }
     }
 }
